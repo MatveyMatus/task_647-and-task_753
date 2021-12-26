fadacd7b99b445421e23efe57d9007df6e56ba5b
MatveyMatus <91337034+MatveyMatus@users.noreply.github.com>
2021-12-26 22:40:33 +0500

    Add files via upload

---
+++ task_647/task_647.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+struct num
+{
+	int val;
+	int pos;
+};
+
+int main() {
+
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	int N, M;
+	in >> N;
+	in >> M;
+
+	num* A = new num[N];
+	for (int i = 1; i <= N; i++)
+		A[i] = { i, i };
+
+	int X;
+	vector<int> V;
+	for (int i = 0; i < M; i++) {
+		in >> X;
+		V.push_back(X);
+	}
+
+	int h;
+	for (int i = 0; i < M; i++) {
+
+		for (int j = 1; j <= N; j++)
+			if (A[j].val == V[i]) {
+				h = A[j].pos;
+				break;
+			}
+
+		for (int j = h - 1; j > 0; j--) {
+			A[j + 1].val = A[j].val;
+			A[j + 1].pos = A[j].pos + 1;
+
+		}
+
+		A[1] = { V[i], 1 };
+
+		out << h << " ";
+
+	}
+
+}
---
+++ task_753/task_753.cpp
@@ -0,0 +1,58 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+string sub(int s, string sr) {
+	string substr;
+	int len_S = s;
+	for (int i = 0; i <= len_S; ++i) {
+		substr += sr[i];
+	}
+	return substr;
+}
+
+int main()
+{
+	string str;
+	int coun = 0;
+	bool bFlag = true;
+	ifstream input("input.txt");
+	ofstream output("output.txt");
+	ofstream substr("substring.txt");
+	getline(input, str);
+	for (int i = 0; i < str.length(); ++i) {
+		for (int j = 0; j < str.length(); ++j) {
+			if (str[j] != str[i]){
+				bFlag = false;
+				break;
+			}
+		}
+	}
+	for (int i = 0; i < str.length(); ++i) {
+		if (bFlag == false) {
+			coun++;
+			sub(i, str);
+			if (i == str.length() - 1) {
+				substr << str << endl;
+				str.erase(0, 1);
+				i = -1;
+			}
+		}
+		else {
+			sub(i, str);
+			if (i == str.length() - 1) {
+				substr << str << endl;
+				str.erase(0, 1);
+				coun++;
+				i = -1;
+			}
+		}
+	}
+	output << coun;
+	input.close();
+	output.close();
+	substr.close();
+	cout << "Everything went well";
+}
